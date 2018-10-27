3caa9278ff0b029752f15ae0db0a081cbd62dc85
Erick Suárez <ericksuarezdev@gmail.com>
2018-10-27 16:19:41 -0500

    Ajuste de rutas

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 //import App from './App';
 //import * as serviceWorker from './serviceWorker';
-import App from './components/app'
+import App from './Ejemplo/app'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
