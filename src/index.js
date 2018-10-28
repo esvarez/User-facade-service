3d1cfee989c140c27ea9f639230fa4a983324553
maurihed <maurihed@gmail.com>
2018-10-28 00:18:00 -0500

    boceto de los componentes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 //import App from './App';
 //import * as serviceWorker from './serviceWorker';
-import App from './Ejemplo/app'
+import CardDetail from './components/CardDetail/CardDetail.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<CardDetail />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
