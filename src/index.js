ed7c24073a654b3393f4c3a09dc43d3531a0a57d
maurihed <maurihed@gmail.com>
2018-10-28 02:27:34 -0600

    nueva estructura

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-//import * as serviceWorker from './serviceWorker';
-//import App from './Ejemplo/app'
-import MapMx from './components/Map/Map'
-import Filter from './components/Filter/Filter'
-import CardDetail from './components/CardDetail/CardDetail'
+import App from './pages/containers/home';
+//data
+import cities from './data/cities.json';
 
-ReactDOM.render(
-    <div class="container">
-    <Filter />
-    <CardDetail />
-        <MapMx />
-    </div>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+ReactDOM.render(<App cities={cities} />, document.getElementById('root'));
