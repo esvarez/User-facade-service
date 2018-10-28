34a401ff41e785c52a9d954e0ee45bd4a44180a3
maurihed <maurihed@gmail.com>
2018-10-28 15:14:08 -0600

    la dona

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import BarDonout from './components/charts/barDonout';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BarDonout />, document.getElementById('root'));
