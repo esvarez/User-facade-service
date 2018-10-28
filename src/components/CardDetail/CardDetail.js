a8eedfece059d81ef992fce50de5078abdda32b4
Erick Suárez <ericksuarezdev@gmail.com>
2018-10-28 05:03:33 -0600

    Botones

--- src/components/CardDetail/CardDetail.js
+++ src/components/CardDetail/CardDetail.js
@@ -7,6 +7,7 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 
 function CardDetail(){
@@ -15,30 +16,40 @@ function CardDetail(){
             <CardHeader title="Informacion de la calidad del aire"/>
             <CardContent>
             <Grid container spacing={16}>
-                <Grid item xs={8} sm={4}>
-                    <Typography component="p">
-                    <strong>Nivel de CO2:</strong> 1.3232
-                    </Typography>
+                <Grid item xs={12} sm={4}>
+                    <Button variant="outlined" color="primary" >
+                        Dióxido de azufre<strong>(SO2)</strong>
+                    </Button>                    
                 </Grid>
-                <Grid item xs={8} sm={4}>
-                    <Typography component="p">
-                    <strong>Nivel de CO2:</strong> 1.3232
-                    </Typography>
+                <Grid item xs={12} sm={4}>
+                    <Button variant="outlined" color="primary" >
+                        Monóxido de carbono<strong>(CO)</strong>
+                    </Button>                    
                 </Grid>
-                <Grid item xs={8} sm={4}>
-                    <Typography component="p">
-                    <strong>Nivel de CO2:</strong> 1.3232
-                    </Typography>
+                <Grid item xs={12} sm={4}>
+                    <Button variant="outlined" color="primary" >
+                        Ozono<strong>(O3)</strong>
+                    </Button>                                    
                 </Grid>
-                <Grid item xs={8} sm={4}>
-                    <Typography component="p">
-                    <strong>Nivel de CO2:</strong> 1.3232
-                    </Typography>
+                <Grid item xs={12} sm={4}>
+                    <Button variant="outlined" color="primary" >
+                        Particulas Menores a 2.5<strong>(PM2.5)</strong>
+                    </Button>                    
                 </Grid>
-                <Grid item xs={8} sm={4}>
-                    <Typography component="p">
-                    <strong>Nivel de CO2:</strong> 1.3232
-                    </Typography>
+                <Grid item xs={12} sm={4}>
+                    <Button variant="outlined" color="primary" >
+                        Dióxido de nitrógeno <strong>(NO2)</strong>
+                    </Button>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Button variant="outlined" color="primary" >
+                        Temperatura<strong>°C</strong>
+                    </Button>                                        
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Button variant="outlined" color="primary" >
+                        Particulas Menores a 10<strong>(PM10)</strong>
+                    </Button>                    
                 </Grid>
             </Grid>
             </CardContent>
--- src/components/GeoContainer.jsx
+++ src/components/GeoContainer.jsx
@@ -4,13 +4,8 @@ import Filter from './Filter/Filter'
 import CardDetail from './CardDetail/CardDetail'
 
 const GeoContainer = (props) => (
-<<<<<<< HEAD
     <div className="container">             
         <Filter 
-=======
-    <div className="container">
-        <Filter
->>>>>>> ed4b5b81a57d7690975e861f5b3b2ff8d78c9714
             cities = {props.cities}
             factors = {props.factors}
             citySelected = {props.citySelected}
