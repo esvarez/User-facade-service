612192f0733c220f271637f5a3ac508af64bd1b1
maurihed <maurihed@gmail.com>
2018-10-28 04:50:40 -0600

    navbar

--- src/components/GeoContainer.jsx
+++ src/components/GeoContainer.jsx
@@ -2,22 +2,30 @@ import React from 'react'
 import MapMx from './Map/Map'
 import Filter from './Filter/Filter'
 import CardDetail from './CardDetail/CardDetail'
+import NavBar from './NavBar'
 
 const GeoContainer = (props) => (
+<div>
+    <NavBar/>
     <div className="container">
         <Filter
+            className="space-around"
             cities = {props.cities}
             factors = {props.factors}
             citySelected = {props.citySelected}
             onChangeCity = { props.onChangeCity }
         />
-        <CardDetail />
+        <CardDetail
+        className="space-around"
+         />
         <MapMx
+            className="space-around"
             citySelected = {props.citySelected}
             position = { props.position }
             zoom = { props.zoom }
         />
     </div>
+</div>
 )
 
 export default GeoContainer
